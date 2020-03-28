a7e026cc8382b953107cc76ab8deab5ee6612c26
ryuvi <vicenters10@gmail.com>
2020-03-28 11:46:11 -0300

    Tests

--- BackEnd/src/controllers/OngController.js
+++ BackEnd/src/controllers/OngController.js
@@ -1,5 +1,4 @@
-const crypto = require('crypto');
-
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 
@@ -18,7 +17,7 @@ module.exports = {
             uf
         } = req.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
